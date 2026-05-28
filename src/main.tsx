549247556d02d5cea1c1587a7492f98fcd3f1d73
QuyetThangDev <quyetthang0501@gmail.com>
2026-05-28 15:23:13 +0700

    fix: self-host Be Vietnam Pro via @fontsource, remove Google Fonts CDN dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/be-vietnam-pro/300.css'
+import '@fontsource/be-vietnam-pro/400.css'
+import '@fontsource/be-vietnam-pro/400-italic.css'
+import '@fontsource/be-vietnam-pro/500.css'
+import '@fontsource/be-vietnam-pro/600.css'
+import '@fontsource/be-vietnam-pro/700.css'
 import './index.css'
 import App from './App.tsx'
 

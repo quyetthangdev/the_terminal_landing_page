21a76c9cec3b194b44caa89da728e54418c82f36
QuyetThangDev <quyetthang0501@gmail.com>
2026-05-28 15:12:55 +0700

    feat: wire /staff routes into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MuseumPage from '@/pages/MuseumPage'
 import GlassPage from '@/pages/GlassPage'
+import StaffFloorPlanPage from '@/pages/staff-floor-plan'
+import StaffTableOrderPage from '@/pages/staff-table-order'
+import StaffPaymentPage from '@/pages/staff-payment'
 
 export default function App() {
   return (
@@ -8,6 +11,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MuseumPage />} />
         <Route path="/glass" element={<GlassPage />} />
+        <Route path="/staff" element={<StaffFloorPlanPage />} />
+        <Route path="/staff/table/:id" element={<StaffTableOrderPage />} />
+        <Route path="/staff/table/:id/payment" element={<StaffPaymentPage />} />
       </Routes>
     </BrowserRouter>
   )
